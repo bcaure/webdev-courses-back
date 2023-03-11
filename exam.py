0f01383951e3e603daeab9cfa1f09112c78fcd8a
Benjamin Caure <bcaure@gmail.com>
2023-03-11 21:21:27 +0100

    :recycle: small modification for 2023 exam

--- exam.py
+++ exam.py
@@ -1,6 +1,5 @@
+# pylint: disable = missing-module-docstring, missing-function-docstring, line-too-long, bare-except
 from flask import jsonify, request, render_template
-from random import randrange
-from datetime import datetime
 from flask_app import app
 from init_db import get_db
 from common import validate_payload
@@ -69,25 +68,25 @@ def exam_post(api_code):
     payload_object = request.get_json(force=True, silent=True)
     try:
         number1 = float(payload_object[columns[1]])
-    except:
-        return f'Field {columns[1]} is not a valid number: {payload_object[columns[1]]}', 400
+    except ValueError:
+        return jsonify({ "message": f'Field {columns[1]} is not a valid number: {payload_object[columns[1]]}'}), 400
     try:
         number2 = float(payload_object[columns[2]])
-    except:
-        return f'Field {columns[2]} is not a valid number: {payload_object[columns[2]]}', 400
+    except ValueError:
+        return jsonify({ "message": f'Field {columns[2]} is not a valid number: {payload_object[columns[2]]}'}), 400
 
     con = get_db()
     cur = con.cursor()
     cur.execute(f'SELECT * FROM {api_code} WHERE {columns[0]} = ?', [payload_object[columns[0]]])
     rows = cur.fetchall()
-    if (len(rows) > 0):
-        return f'Impossible to create a new record with identical field {columns[0]}: the value "{payload_object[columns[0]]}" already exists', 400
+    if len(rows) > 0:
+        return jsonify({ "message": f'Impossible to create a new record with identical field {columns[0]}: the value "{payload_object[columns[0]]}" already exists'}), 400
 
     cur.execute(f"INSERT INTO {api_code}({columns[0]}, {columns[1]}, {columns[2]}) VALUES (?, ?, ?)",
         (payload_object[columns[0]], number1, number2))
     result = cur.rowcount
     con.commit()
-    return f'{result} ROW(S) INSERTED' 
+    return jsonify({ "message": f'{result} ROW(S) INSERTED'})
 
 @app.route('/exam/api/<api_code>', methods=['PUT'])
 def exam_put(api_code):
@@ -98,35 +97,51 @@ def exam_put(api_code):
     payload_object = request.get_json(force=True, silent=True)
     try:
         number1 = float(payload_object[columns[1]])
-    except:
-        return f'Field {columns[1]} is not a valid number: {payload_object[columns[1]]}', 400
+    except ValueError:
+        error_msg = {
+            "message": f'Field {columns[1]} is not a valid number: {payload_object[columns[1]]}'
+        }
+        return jsonify(error_msg), 400
     try:
         number2 = float(payload_object[columns[2]])
-    except:
-        return f'Field {columns[2]} is not a valid number: {payload_object[columns[2]]}', 400
+    except ValueError:
+        error_msg = {
+            "message": f'Field {columns[2]} is not a valid number: {payload_object[columns[2]]}'
+        }
+        return jsonify(error_msg), 400
 
     con = get_db()
     cur = con.cursor()
     cur.execute(f'SELECT * FROM {api_code} WHERE {columns[0]} = ?', [payload_object[columns[0]]])
     rows = cur.fetchall()
-    if (len(rows) == 0):
-        return f'Impossible to update an existing record: no record was found with field {columns[0]}="{payload_object[columns[0]]}"', 400
+    if len(rows) == 0:
+        error_field = f'{columns[0]}="{payload_object[columns[0]]}"'
+        error_msg = {
+            "message": f'Impossible to update: no record found with field {error_field}'
+        }
+        return jsonify(error_msg), 400
 
     cur.execute(f"UPDATE {api_code} SET {columns[1]}=?, {columns[2]}=? WHERE {columns[0]} = ?",
         (number1, number2, payload_object[columns[0]]))
     result = cur.rowcount
     con.commit()
-    return f'{result} ROW(S) UPDATED'
+    return jsonify({ "message": f'{result} ROW(S) UPDATED'})
 
 @app.route('/exam/student/<student_number>')
 def student(student_number):
     number = int(student_number)
     api_code = code(number)
     columns = COLUMNS_LIST[index_of_code(api_code)]
-    question1 = 'list' if number % 2 == 1 else 'table'
-    question2 = 'max' if number % 4 == 0 or number % 4 == 1 else 'mean'
-    question3 = 'insert' if number % 8 == 0 or number % 8 == 1 or number % 8 == 2  or number % 8 == 3 else 'update'
-    return render_template('exam.html', title=title(number), code=api_code, columns=columns, question1=question1, question2=question2, question3=question3)
-
-
-
+    questions = [
+        ["table", "mean", "insert"],
+        ["table", "mean", "update"],
+        ["table", "max", "insert"],
+        ["table", "max", "update"],
+        ["list", "mean", "insert"],
+        ["list", "mean", "update"],
+        ["list", "max", "insert"],
+        ["list", "max", "update"],
+    ]
+    idx = number % 8
+    questions = questions[idx]
+    return render_template('exam.html', title=title(number), code=api_code, columns=columns, question1=questions[0], question2=questions[1], question3=questions[2])
